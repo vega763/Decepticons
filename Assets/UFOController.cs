81afea73ff941316cea898ec035f83252643fb29
christian <vega763@minidokaschools.org>
2019-04-16 11:04:07 -0600

    ufo

--- Assets/UFOController.cs
+++ Assets/UFOController.cs
@@ -6,10 +6,11 @@ public class UFOController : MonoBehaviour {
 
     //Add Variables here
     public Rigidbody2D theRB2d;
-
+    public float movementSpeed;
 
 	// Use this for initialization
 	void Start () {
+        theRB2d = GetComponent<Rigidbody2D>();
 		
 	}
 	
@@ -17,6 +18,22 @@ public class UFOController : MonoBehaviour {
 	void Update () {
 
 		//Add Movement Code Here
-		
-	}
+		if(Input.GetKeyDown(KeyCode.W))
+        {
+            theRB2d.velocity += new Vector2(0f,1.0f)*movementSpeed;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            theRB2d.velocity += new Vector2(0.0f, -1.0f) * movementSpeed;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            theRB2d.velocity += new Vector2(-1.0f,0.0f) * movementSpeed;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            theRB2d.velocity += new Vector2(1.0f, 0.0f) * movementSpeed;
+        }
+
+    }
 }
