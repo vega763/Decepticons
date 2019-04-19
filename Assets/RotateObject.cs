fec8ac1057fb27427491327e11b633a56caf1002
mike <johns456@minidokaschools.org>
2019-04-19 11:10:08 -0600

    gold

---
+++ Assets/RotateObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public float rotatespeed;
+    public bool allowRotate; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (allowRotate)
+        {
+            transform.Rotate(Vector3.forward, rotatespeed * Time.deltaTime, Space.World);
+        }
+
+
+
+
+    }
+}
