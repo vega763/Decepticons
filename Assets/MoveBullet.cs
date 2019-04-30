8addd483153db4ebc4d30a8afadef4d1a2f61653
mike <liquegf>
2019-04-30 11:11:20 -0600

    bullet

---
+++ Assets/MoveBullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBullet : MonoBehaviour
+{
+    public float movementSpeed;
+    public Rigidbody2D theRB2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB2D.velocity = transform.right * movementSpeed;
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag != "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
