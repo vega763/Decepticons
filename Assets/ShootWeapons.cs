5c04890ad7587c79f97c308748280e5ec2f012d1
Mikey <beech731@minidokaschools.org>
2019-05-03 11:16:01 -0600

    Update ShootWeapons.cs

--- Assets/ShootWeapons.cs
+++ Assets/ShootWeapons.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class ShootWeapons : MonoBehaviour
 {
-    public GameObject bullet;
-    public Transform barrel;
-    // Start is called before the first frame update
+    /*
+    public GameObject barrel;
+    public LaserSightControll LzrSight;
+    public float speed;
+    public float angle;
+
+    public UFOController myUFO;
+    public GameObject projectile;
+    // use this for initialization
+    void start() {
+     myUFO = GetComponent<UFOController>();
+
+    }
+
+ 
+
+
     void Start()
     {
         
@@ -15,9 +29,38 @@ public class ShootWeapons : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        shootprojectile();
+    }
+
+    public void ShootProjectile()
+    {
+        //Rotate Laser
+        Vector3 dir = Input.mousePosition -
+        Camera.main.WorldToScreenPoint(transform.position);
+        angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Dog;
+        barrel.transformation.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        //Set laser endpoint
+        dir = Camera.main.ScreenToWorldPoint(Inmput.mouseposition);
+        dir.z = 0f;
+        LzrSight.Setend(dir);
+        //toggle laser 
+        if (Input.GetMouseButtonDown(1) || Input.GetKeyDown (KeyCode.Z))
+        {
+
+            LzrSight.togglelazer();
+}
+        //Shoot projectile
         if(Input.GetMouseButtonDown(0))
         {
-            Instantiate(bullet, barrel.position, barrel.rotation);
-        }
+            dir = Imput.mousePosition -
+                Camera.main.WorldToScreenPoint(transform.position);
+            angle = Mathf.Atan2(dir.y, dir.x) * mthf.Rad2Deg;
+
+            barrel.transformation.rotation = quaternion.angleaxis(angle, Vector3.forward);
+            Insatntiate(projectile, transform.position, barrel.trasform.rotation);
+}
     }
+    */
 }
