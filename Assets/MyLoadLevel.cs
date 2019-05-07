eb67d4a3bbfe243a7e3253ee2436cc0a92603ec1
Mikey <beech731@minidokaschools.org>
2019-05-07 10:45:08 -0600

    cv

---
+++ Assets/MyLoadLevel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MyLoadLevel : MonoBehaviour
+{
+    public string LevelToLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+
+
+
+            //Application.LoadLevel(LevelToLoad);
+            SceneManager.LoadScene(LevelToLoad);
+        }
+    }
+}
